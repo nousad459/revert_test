7fabcd214921fbdaa33cf5f1ed33e848117f6dee
nousad459 <57261011+nousad459@users.noreply.github.com>
2020-07-18 16:54:27 +0530

    Update remeberme.js

--- remeberme.js
+++ remeberme.js
@@ -1,79 +1,88 @@
-import React, { useState, useEffect } from 'react'; 
- function RememberMe(props){
-    // static displayName = 'RememberMe'
- 
-    // state = {
-    //     email: '',
-    //     password: '',
-    //     isChecked: false,
-    // }
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isChecked, setisChecked] = useState(false);
+import React, { useState, useEffect } from "react";
+function RememberMe(props) {
+  // static displayName = 'RememberMe'
 
- 
-    useEffect(()=> {
-        if (localStorage.checkbox && localStorage.email !== "") {
-            // this.setState({
-            //     isChecked: true,
-            //     email: localStorage.username,
-            //     password: localStorage.password
-            // })
-            setEmail(localStorage.username)
-            setPassword(localStorage.password)
-            setisChecked(true)
-        }
-    })
- 
-   const onChangeValue = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+  // state = {
+  //     email: '',
+  //     password: '',
+  //     isChecked: false,
+  // }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setisChecked] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.checkbox && localStorage.email !== "") {
+      setEmail(localStorage.username);
+      setPassword(localStorage.password);
+      setisChecked(true);
     }
- 
-  const  onChangeCheckbox = event => {
-        this.setState({
-            isChecked: event.target.checked
-        })
+    console.log(isChecked);
+  },[]);
+
+  const onChangeCheckbox = (event) => {
+    setisChecked(event.target.checked);
+  };
+
+  const loginSubmit = () => {
+    if (isChecked && email !== "") {
+      localStorage.username = email;
+      localStorage.password = password;
+      localStorage.checkbox = isChecked;
     }
- 
-   const loginSubmit = () => {
-        //const { email, password, isChecked } = this.state
-        if (isChecked && email !== "") {
-            localStorage.username = email
-            localStorage.password = password
-            localStorage.checkbox = isChecked
-        }
-        // here call the API to signup/login
-    }
- 
-    // render() {
-    //     const { email, password, isChecked } = this.state
-        return (
-            <div>
-                <form>
-                    <table align="center">
-                        <tr>
-                            <td><label>Email</label></td>
-                            <td><input type="email" name="email" value={email} onChange={onChangeValue}/></td>
-                        </tr>
-                        <tr>
-                            <td><label>Password</label></td>
-                            <td><input type="password" name="password" value={password} onChange={onChangeValue}/></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">
-                                <input type="checkbox" checked={isChecked} name="lsRememberMe" onChange={onChangeCheckbox} />
-                                <label>Remember me</label></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2"><input type="button" value="Login" onClick={loginSubmit} /></td>
-                        </tr>
-                    </table>
-                </form>
-            </div>
-        )
-    // }
+  };
+
+  return (
+    <div>
+      <form>
+        <table align="center">
+          <tr>
+            <td>
+              <label>Email</label>
+            </td>
+            <td>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Password</label>
+            </td>
+            <td>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <input
+                type="checkbox"
+                checked={isChecked}
+                name="lsRememberMe"
+                onChange={onChangeCheckbox}
+              />
+              <label>Remember me</label>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <input type="button" value="Login" onClick={loginSubmit} />
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  );
+  // }
 }
- 
-export default RememberMe
+
+export default RememberMe;
